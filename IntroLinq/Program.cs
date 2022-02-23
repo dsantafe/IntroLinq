899497d0799a43d192a1f38f71eb2fd0794b2b22
David Santafe <santafedavid01@gmail.com>
2022-02-22 19:54:16 -0500

    Ejercicios Linq

--- IntroLinq/Program.cs
+++ IntroLinq/Program.cs
@@ -1,5 +1,6 @@
 ﻿using IntroLinq;
 
+#region Linq Parte 1
 var numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
 
 //var sum = 0.0;
@@ -32,21 +33,47 @@ var mayorQue5 = from x in numbers
 
 var entre5y7 = from x in numbers
                where x >= 5 && x <= 7
-               select x;
+               select x; 
+#endregion
 
 var books = Book.Books();
 var authors = Author.Books();
 
-foreach (var item in books)
-    Console.WriteLine(item.Title);
+//Mostrar en consola los 3 libros con más ventas. *
+//Mostrar en consola los 3 libros con menos ventas. *
+//Mostrar en consola el autor con más libros publicados.
+//Mostrar en consola el autor y la cantidad de libros publicados. *
+//Mostrar en consola los libros publicados hace menos de 50 años. *
+//Mostrar en consola el libro más viejo. *
+//Mostrar en consola los autores que tengan un libro que comience con "El". *
 
-var query1 = from x in books
-             where x.PublicationDate > 1900
-             select x;
+var ex1 = books.OrderByDescending(x => x.Sales).Take(3).ToList();
+var ex2 = books.OrderBy(x => x.Sales).Take(3).ToList();
+var ex3 = books.Where(x => x.PublicationDate > DateTime.Now.AddYears(-50).Year).ToList();
+var ex4 = books.OrderBy(x => x.PublicationDate).FirstOrDefault();
+var ex5 = from b in books
+          join a in authors on b.AuthorId equals a.AuthorId
+          where b.Title.ToLower().StartsWith("el")
+          select a;
+var ex6 = from b in books
+          join a in authors on b.AuthorId equals a.AuthorId
+          group a by a.Name into query
+          select query;
 
-query1 = books.Where(x => x.PublicationDate > 1900).Select(x => x);
+foreach (var item in ex6)
+    Console.WriteLine("Autor: " + item.Key + "Cantidad: " + item.Count());
 
+foreach (var item in ex1)
+    Console.WriteLine($"{item.Title} {item.Sales}");
 
-
+/*
+    SELECT a.Name, Count(*)
+    FROM Book b
+    JOIN Author a ON b.AuthorId = a.AuthorId
+    GROUP BY a.Name
+    
+    Glenn Cooper    3
+    Dann Brown      2
+ */
 
 
